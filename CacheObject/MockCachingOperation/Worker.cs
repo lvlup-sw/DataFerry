752171a64636d0fc9423e3e18c8ba2a9b24719c8
Reed Salus <reedsalus@gmail.com>
2024-02-26 01:59:28 -0500

    cleanup

--- CacheObject/MockCachingOperation/Worker.cs
+++ CacheObject/MockCachingOperation/Worker.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MockCachingOperation
 {
@@ -15,7 +10,7 @@ namespace MockCachingOperation
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                await Task.Run(() => Console.WriteLine($"Beginning caching operation: {DateTime.Now}\n"), stoppingToken);
+                Console.WriteLine($"Beginning caching operation: {DateTime.Now}\n");
                 await _mockCachingOperation.StartAsync(stoppingToken);
             }
         }
