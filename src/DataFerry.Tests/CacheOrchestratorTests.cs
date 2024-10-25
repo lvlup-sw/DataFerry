9955ae4a0acb3dff9cf24f25c5c9b582f786bfee
Reed Salus <salusreed@microsoft.com>
2024-10-25 06:39:43 -0700

    batch sections for tests

--- src/DataFerry.Tests/CacheOrchestratorTests.cs
+++ src/DataFerry.Tests/CacheOrchestratorTests.cs
@@ -299,7 +299,7 @@ namespace lvlup.DataFerry.Tests
         #endregion
 
         #region ASYNCHRONOUS OPERATIONS
-        #region GETASYNC
+        #region GET ASYNC
 
         [DataTestMethod]
         [DataRow("testKey")]
@@ -316,12 +316,227 @@ namespace lvlup.DataFerry.Tests
             var destination = new ArrayBufferWriter<byte>();
 
             // Act
-            _cache.GetFromCache(key, destination);
+            await _cache.GetFromCacheAsync(key, destination);
 
             // Assert
             _memCache.Verify(cache => cache.TryGet(key, out serializedByte), Times.Once);
             CollectionAssert.AreEqual(serializedByte, destination.WrittenMemory.ToArray());
         }
+
+        [DataTestMethod]
+        [DataRow("testKey")]
+        [DataRow("payload123")]
+        [DataRow("levelupsoftware")]
+        [TestMethod]
+        public async Task GetFromCacheAsync_KeyNotInMemCache_ButInRedis_ReturnsValue(string key)
+        {
+            // Arrange
+            Payload expectedValue = TestUtils.CreatePayloadWithInput(key);
+            byte[] serializedByte = JsonSerializer.SerializeToUtf8Bytes(expectedValue);
+
+            RedisValue redisSerializedValue = (RedisValue)serializedByte;
+            byte[]? nullValue = default;
+            _memCache.Setup(cache => cache.TryGet(key, out nullValue)).Returns(false);
+            Mock<IDatabase> database = new();
+            database.Setup(cache => cache.StringGetAsync(key, CommandFlags.PreferReplica)).ReturnsAsync(redisSerializedValue);
+            _redis.Setup(db => db.GetDatabase(It.IsAny<int>(), It.IsAny<object>())).Returns(database.Object);
+            var destination = new ArrayBufferWriter<byte>();
+
+            // Act: Call the method under test
+            await _cache.GetFromCacheAsync(key, destination);
+
+            // Assert: Verify the result
+            _memCache.Verify(cache => cache.TryGet(key, out nullValue), Times.Once);
+            database.Verify(cache => cache.StringGetAsync(key, CommandFlags.PreferReplica), Times.Once);
+            CollectionAssert.AreEqual(serializedByte, destination.WrittenMemory.ToArray());
+        }
+
+        [DataTestMethod]
+        [DataRow("testKey")]
+        [DataRow("payload123")]
+        [DataRow("levelupsoftware")]
+        [TestMethod]
+        public async Task GetFromCacheAsync_KeyNotFound_ReturnsNull(string key)
+        {
+            // Arrange
+            Payload expectedValue = TestUtils.CreatePayloadWithInput(key);
+            byte[] serializedByte = JsonSerializer.SerializeToUtf8Bytes(expectedValue);
+
+            byte[]? nullValue = default;
+            _memCache.Setup(cache => cache.TryGet(key, out nullValue)).Returns(false);
+            Mock<IDatabase> database = new();
+            database.Setup(cache => cache.StringGetAsync(key, CommandFlags.PreferReplica)).ReturnsAsync(RedisValue.Null);
+            _redis.Setup(db => db.GetDatabase(It.IsAny<int>(), It.IsAny<object>())).Returns(database.Object);
+            var destination = new ArrayBufferWriter<byte>();
+
+            // Act: Call the method under test
+            await _cache.GetFromCacheAsync(key, destination);
+
+            // Assert: Verify the result
+            _memCache.Verify(cache => cache.TryGet(key, out nullValue), Times.Once);
+            database.Verify(cache => cache.StringGetAsync(key, CommandFlags.PreferReplica), Times.Once);
+            CollectionAssert.AreNotEqual(serializedByte, destination.WrittenMemory.ToArray());
+            Assert.IsTrue(destination.FreeCapacity == destination.WrittenSpan.Length);
+        }
+
+        #endregion
+        #region SET ASYNC
+
+        [DataTestMethod]
+        [DataRow("testKey")]
+        [DataRow("payload123")]
+        [DataRow("levelupsoftware")]
+        [TestMethod]
+        public async Task SetInCacheAsync_SetsValuesInCaches_AndReturnsTrue(string key)
+        {
+            // Arrange
+            Payload expectedValue = TestUtils.CreatePayloadWithInput(key);
+            byte[] serializedValue = JsonSerializer.SerializeToUtf8Bytes(expectedValue);
+
+            var options = new DistributedCacheEntryOptions { SlidingExpiration = TimeSpan.FromMinutes(5) };
+            Mock<IDatabase> database = new();
+            database.Setup(cache => cache.StringSetAsync(key, serializedValue, TimeSpan.FromMinutes(5), false, When.Always, CommandFlags.None)).ReturnsAsync(true);
+            _redis.Setup(db => db.GetDatabase(It.IsAny<int>(), It.IsAny<object>())).Returns(database.Object);
+
+            // Act
+            bool result = await _cache.SetInCacheAsync(key, serializedValue, options);
+
+            // Assert
+            Assert.IsTrue(result);
+            _memCache.Verify(x => x.CheckBackplane(), Times.Once);
+            _memCache.Verify(x => x.AddOrUpdate(key, serializedValue, TimeSpan.FromMinutes(_settings.Value.InMemoryAbsoluteExpiration)), Times.Once);
+            database.Verify(x => x.StringSetAsync(key, serializedValue, TimeSpan.FromMinutes(5), It.IsAny<bool>(), It.IsAny<When>(), It.IsAny<CommandFlags>()), Times.Once);
+        }
+
+        [DataTestMethod]
+        [DataRow("testKey")]
+        [DataRow("payload123")]
+        [DataRow("levelupsoftware")]
+        [TestMethod]
+        public async Task SetInCacheAsync_RemoteCacheOperationFails_AndReturnsFalse(string key)
+        {
+            // Arrange
+            Payload expectedValue = TestUtils.CreatePayloadWithInput(key);
+            byte[] serializedValue = JsonSerializer.SerializeToUtf8Bytes(expectedValue);
+
+            var options = new DistributedCacheEntryOptions { SlidingExpiration = TimeSpan.FromMinutes(5) };
+            Mock<IDatabase> database = new();
+            database.Setup(cache => cache.StringSetAsync(key, serializedValue, TimeSpan.FromMinutes(5), false, When.Always, CommandFlags.None)).ReturnsAsync(false);
+            _redis.Setup(db => db.GetDatabase(It.IsAny<int>(), It.IsAny<object>())).Returns(database.Object);
+
+            // Act
+            bool result = await _cache.SetInCacheAsync(key, serializedValue, options);
+
+            // Assert
+            Assert.IsFalse(result);
+            _memCache.Verify(x => x.CheckBackplane(), Times.Once);
+            _memCache.Verify(x => x.AddOrUpdate(key, serializedValue, TimeSpan.FromMinutes(_settings.Value.InMemoryAbsoluteExpiration)), Times.Once);
+            database.Verify(x => x.StringSetAsync(key, serializedValue, TimeSpan.FromMinutes(5), It.IsAny<bool>(), It.IsAny<When>(), It.IsAny<CommandFlags>()), Times.Once);
+        }
+
+        #endregion
+        #region REFRESH ASYNC
+
+        [DataTestMethod]
+        [DataRow("testKey")]
+        [DataRow("payload123")]
+        [DataRow("levelupsoftware")]
+        [TestMethod]
+        public async Task RefreshInCacheAsync_RefreshesValuesInCaches_AndReturnsTrue(string key)
+        {
+            // Arrange
+            TimeSpan ttl = TimeSpan.FromMinutes(60);
+
+            Mock<IDatabase> database = new();
+            database.Setup(cache => cache.KeyExpireAsync(key, ttl, ExpireWhen.Always, CommandFlags.None)).ReturnsAsync(true);
+            _redis.Setup(db => db.GetDatabase(It.IsAny<int>(), It.IsAny<object>())).Returns(database.Object);
+
+            // Act
+            bool result = await _cache.RefreshInCacheAsync(key, ttl);
+
+            // Assert
+            Assert.IsTrue(result);
+            _memCache.Verify(x => x.CheckBackplane(), Times.Once);
+            _memCache.Verify(x => x.Refresh(key, TimeSpan.FromMinutes(_settings.Value.InMemoryAbsoluteExpiration)), Times.Once);
+            database.Verify(x => x.KeyExpireAsync(key, ttl, ExpireWhen.Always, It.IsAny<CommandFlags>()), Times.Once);
+        }
+
+        [DataTestMethod]
+        [DataRow("testKey")]
+        [DataRow("payload123")]
+        [DataRow("levelupsoftware")]
+        [TestMethod]
+        public async Task RefreshInCacheAsync_RemoteCacheOperationFails_AndReturnsFalse(string key)
+        {
+            // Arrange
+            TimeSpan ttl = TimeSpan.FromMinutes(60);
+
+            Mock<IDatabase> database = new();
+            database.Setup(cache => cache.KeyExpireAsync(key, ttl, ExpireWhen.Always, CommandFlags.None)).ReturnsAsync(false);
+            _redis.Setup(db => db.GetDatabase(It.IsAny<int>(), It.IsAny<object>())).Returns(database.Object);
+
+            // Act
+            bool result = await _cache.RefreshInCacheAsync(key, ttl);
+
+            // Assert
+            Assert.IsFalse(result);
+            _memCache.Verify(x => x.CheckBackplane(), Times.Once);
+            _memCache.Verify(x => x.Refresh(key, TimeSpan.FromMinutes(_settings.Value.InMemoryAbsoluteExpiration)), Times.Once);
+            database.Verify(x => x.KeyExpireAsync(key, ttl, ExpireWhen.Always, It.IsAny<CommandFlags>()), Times.Once);
+        }
+
+        #endregion
+        #region REMOVE ASYNC
+
+        [DataTestMethod]
+        [DataRow("testKey")]
+        [DataRow("payload123")]
+        [DataRow("levelupsoftware")]
+        [TestMethod]
+        public async Task RemoveInCacheAsync_RemovesValuesInCaches_AndReturnsTrue(string key)
+        {
+            // Arrange
+            Mock<IDatabase> database = new();
+            database.Setup(cache => cache.KeyDeleteAsync(key, CommandFlags.None)).ReturnsAsync(true);
+            _redis.Setup(db => db.GetDatabase(It.IsAny<int>(), It.IsAny<object>())).Returns(database.Object);
+
+            // Act
+            bool result = await _cache.RemoveFromCacheAsync(key);
+
+            // Assert
+            Assert.IsTrue(result);
+            _memCache.Verify(x => x.CheckBackplane(), Times.Once);
+            _memCache.Verify(x => x.Remove(key), Times.Once);
+            database.Verify(x => x.KeyDeleteAsync(key, It.IsAny<CommandFlags>()), Times.Once);
+        }
+
+        [DataTestMethod]
+        [DataRow("testKey")]
+        [DataRow("payload123")]
+        [DataRow("levelupsoftware")]
+        [TestMethod]
+        public async Task RemoveInCacheAsync_RemoteCacheOperationFails_AndReturnsFalse(string key)
+        {
+            // Arrange
+            Mock<IDatabase> database = new();
+            database.Setup(cache => cache.KeyDeleteAsync(key, CommandFlags.None)).ReturnsAsync(false);
+            _redis.Setup(db => db.GetDatabase(It.IsAny<int>(), It.IsAny<object>())).Returns(database.Object);
+
+            // Act
+            bool result = await _cache.RemoveFromCacheAsync(key);
+
+            // Assert
+            Assert.IsFalse(result);
+            _memCache.Verify(x => x.CheckBackplane(), Times.Once);
+            _memCache.Verify(x => x.Remove(key), Times.Once);
+            database.Verify(x => x.KeyDeleteAsync(key, It.IsAny<CommandFlags>()), Times.Once);
+        }
+
+        #endregion
+        #endregion
+
+        #region ASYNCHRONOUS BATCH OPERATIONS
+        #region GET BATCH ASYNC
 
         #endregion
         #endregion
