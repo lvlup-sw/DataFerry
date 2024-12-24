495d3176f88d3e8cf82340047ae909d462c19800
Reed Salus <reedsalus@gmail.com>
2024-12-23 21:00:32 -0500

    update buffer locking

--- src/DataFerry/Buffers/BoundedBuffer.cs
+++ src/DataFerry/Buffers/BoundedBuffer.cs
@@ -9,6 +9,7 @@ namespace lvlup.DataFerry.Buffers;
 /// <typeparam name="T">Specifies the type of elements in the buffer. Must be a struct.</typeparam>
 public class BoundedBuffer<T> where T : struct
 {
+    // Backing structures
     /// <summary>
     /// The underlying array used as a circular buffer to store the items.
     /// </summary>
@@ -44,7 +45,7 @@ public class BoundedBuffer<T> where T : struct
     /// The current number of items in the buffer.
     /// </summary>
     private int _count;
-    
+
     /// <summary>
     /// The number of times the consumer will spin before waiting on the _notEmptyEvent.
     /// </summary>
@@ -92,30 +93,6 @@ public class BoundedBuffer<T> where T : struct
     }
 
     /// <summary>
-    /// Adds an item to the buffer. If the buffer is full, the calling thread will block until space is available.
-    /// </summary>
-    /// <param name="item">The item to add.</param>
-    public void Add(T item)
-    {
-        lock (@producerLock)
-        {
-            // Wait if buffer is full
-            while (_count == _capacity)
-            {
-                Monitor.Wait(@producerLock);
-            }
-
-            _buffer[_head] = item;
-            _head = (_head + 1) % _capacity;
-            _count++;
-
-            // If buffer has items, signal the consumer
-            if (_count == 1) _notEmptyEvent.Set();
-            Monitor.PulseAll(@producerLock);
-        }
-    }
-
-    /// <summary>
     /// Attempts to remove and return an item from the buffer.
     /// </summary>
     /// <param name="item">When this method returns, contains the item removed from the buffer, if successful; otherwise, the default value of type T.</param>
@@ -141,16 +118,8 @@ public class BoundedBuffer<T> where T : struct
         item = _buffer[_tail];
         _tail = (_tail + 1) % _capacity;
 
-        // Decrement _count and check if we need to signal producers
-        if (Interlocked.Decrement(ref _count) == _capacity - 1)
-        {
-            // Prevent re-ordering
-            Thread.MemoryBarrier();
-            lock (@producerLock)
-            {
-                Monitor.PulseAll(@producerLock);
-            }
-        }
+        // Decrement _count
+        Interlocked.Decrement(ref _count);
 
         // Reset if buffer is empty
         if (_count == 0) _notEmptyEvent.Reset();
@@ -171,16 +140,8 @@ public class BoundedBuffer<T> where T : struct
             yield return _buffer[_tail];
             _tail = (_tail + 1) % _capacity;
 
-            // Decrement _count and check if we need to signal producers
-            if (Interlocked.Decrement(ref _count) == _capacity - 1)
-            {
-                // Prevent re-ordering
-                Thread.MemoryBarrier();
-                lock (@producerLock)
-                {
-                    Monitor.PulseAll(@producerLock);
-                }
-            }
+            // Decrement _count
+            Interlocked.Decrement(ref _count);
         }
 
         _notEmptyEvent.Reset();
@@ -194,16 +155,11 @@ public class BoundedBuffer<T> where T : struct
         _notEmptyEvent.Wait();
 
         // No other consumers, so reading is safe without a lock
-        _tail = _head; // Effectively resets the buffer
-        if (Interlocked.Exchange(ref _count, 0) == _capacity - 1)
-        {
-            // Prevent re-ordering
-            Thread.MemoryBarrier();
-            lock (@producerLock)
-            {
-                Monitor.PulseAll(@producerLock);
-            }
-        }
+        // Effectively resets the buffer
+        _tail = _head;
+
+        // Update count
+        Interlocked.Exchange(ref _count, 0);
 
         _notEmptyEvent.Reset();
     }
